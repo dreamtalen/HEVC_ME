22543c206d3c7e5ba2220702ec3a765efff2e568
Dmtalen Ding <dreamtalen@hotmail.com>
2018-02-25 20:05:56 +0800

    修改bank的端口，让写数据的地址也由外部信号控制

--- ME_DMT/Bank.v
+++ ME_DMT/Bank.v
@@ -12,40 +12,14 @@
 module Bank(
 input clk,
 input rst_n,
-input beg_en,               	 	 //input enable,synchronization with data 
 input [8*`PIXEL-1:0] ref_in, 	     //reference input,8 pixels=64bit
 input Bank_sel,     				 //if Bank_sel=0,enable to storage
 input [6:0] address,			      //according address to select 1 data(8 pixels)
+input [6:0] write_address,
 input rd_en,	
 output [8*`PIXEL-1:0] ref_ou	     //output 1 data(8 pixels) of Bank
 //output reg da_va                 	 //data valid,synchronization with ref_ou
 );
-
-//beg_en   x 1 1 1 1
-//Bank_sel _ 1 1 1 1 
-//ref_in   x D D D
-//me_cnt     0 1 2
-
-//这行删去，下面已经实例化了Memory
-// reg [8*`PIXEL-1:0] mem [95:0]; //to create a memery, width is 64 bit,depth is 96
-
-reg [6:0] me_cnt;            //when the memeroy is full,write again(coverage previous data)
-always@(posedge clk or negedge rst_n)
-begin
-if(!rst_n)
-  me_cnt<=0;
-else if(beg_en)
-begin
-	 if(~Bank_sel)
-		  begin
-		  if(me_cnt<95)       //mem_cnt:0-95
-			 me_cnt<=me_cnt+1;
-		  else 
-			 me_cnt<=0;
-		  end
-		  
-end
-end
 
 S65NLLHSDPH_X40Y4D64 i_ram(
   .QA(ref_ou),
@@ -57,7 +31,7 @@ S65NLLHSDPH_X40Y4D64 i_ram(
   .WENA(1'b1),
   .WENB(Bank_sel),
   .AA({1'b0,address}),
-  .AB({1'b0,me_cnt }),
+  .AB({1'b0,write_address}),
   .DA(),
   .DB(ref_in)
 );
--- ME_DMT/Bank_tb.v
+++ ME_DMT/Bank_tb.v
@@ -7,15 +7,15 @@
 module Bank_tb;
 reg clk;
 reg rst_n;
-reg beg_en;
 reg [8*`PIXEL-1:0] ref_in;
 reg Bank_sel;
 reg [6:0] address;
+reg [6:0] write_address;
 reg rd_en;
 wire [8*`PIXEL-1:0] ref_ou;
 
-Bank bank(.clk(clk), .rst_n(rst_n), .beg_en(beg_en), .ref_in(ref_in), .Bank_sel(Bank_sel),
-	.address(address), .rd_en(rd_en), .ref_ou(ref_ou));
+Bank bank(.clk(clk), .rst_n(rst_n), .ref_in(ref_in), .Bank_sel(Bank_sel),
+	.address(address), .write_address(write_address), .rd_en(rd_en), .ref_ou(ref_ou));
 
 initial
 	clk = 1'b0;
@@ -26,21 +26,20 @@ initial
 begin
 	rst_n = 1'b0;
 	#10 rst_n = 1'b1;
-	// 先从B端口写入一些数据，地址由bank自动生成1，2，..96
-	beg_en = 1'b1;
+	// 已修改，（先从B端口写入一些数据，地址由bank自动生成1，2，..96）
 	Bank_sel = 1'b0;
+	write_address = 7'b0000000;
 	ref_in = { 8{8'b00001111} };
 	#30 ref_in = { 8{8'b01010101} };
+	write_address = 7'b0000001;
 	#30 ref_in = { 8{8'b00110011} };
+	write_address = 7'b0000011;
 	// 再从A端口读出来，地址由用户指定
 	#100 address = 7'b0000000;
 	rd_en = 1'b0;
 	#10 address = 7'b0000001;
 	#10 address = 7'b0000010;
 	#10 address = 7'b0000011;
-	#10 address = 7'b0000100;
-	#10 address = 7'b0000101;
-	#10 address = 7'b0001000;
 end
 
 initial

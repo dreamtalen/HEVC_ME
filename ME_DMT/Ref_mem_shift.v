8251f376fd77fd491bcf0f937a64b660bc69b6cd
Dmtalen Ding <dreamtalen@hotmail.com>
2018-03-01 16:45:54 +0800

    移位*256

--- ME_DMT/Ref_mem_shift.v
+++ ME_DMT/Ref_mem_shift.v
@@ -4,6 +4,6 @@ input [4:0] shift_value, //左移的mem数量
 output wire [2047:0] ref_output
 );
 
-assign ref_output = (ref_input>>shift_value)|(ref_input<<(2048-shift_value));
+assign ref_output = (ref_input>>shift_value*256)|(ref_input<<(2048-shift_value*256));
 
 endmodule

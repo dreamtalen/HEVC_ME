5b7fb33f58697430f27e62782b7a55361435ec92
Dmtalen Ding <dreamtalen@hotmail.com>
2018-04-22 15:20:04 +0800

    change always block to initial

--- ME_DMT/test_rdR_sel.v
+++ ME_DMT/test_rdR_sel.v
@@ -5,17 +5,13 @@ reg clk;
 initial begin
 	clk = 1'b0;
 	rdR_sel = 0;
-	sub_area2_row_count = 0;
+	sub_area2_row_count = 7'd12;
+	#10 rdR_sel = sub_area2_row_count[3:0] - 4'd10;
+	#10 rdR_sel = sub_area2_row_count - 4'd10;
+	#10 sub_area2_row_count = 7'd18;
+	#10 rdR_sel = sub_area2_row_count[3:0] - 4'd10;
+	#10 rdR_sel = sub_area2_row_count - 4'd10;
 end
 always
-	#5 clk = ~clk;
-
-always @(posedge clk) begin
-	sub_area2_row_count <= 7'd12;
-	#10 rdR_sel <= sub_area2_row_count[3:0] - 4'd10;
-	#10 rdR_sel <= sub_area2_row_count - 4'd10;
-	#10 sub_area2_row_count <= 7'd18;
-	#10 rdR_sel <= sub_area2_row_count[3:0] - 4'd10;
-	#10 rdR_sel <= sub_area2_row_count - 4'd10;
-end
+	#5 clk <= ~clk;
 endmodule
